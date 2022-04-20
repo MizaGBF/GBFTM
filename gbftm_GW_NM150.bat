5b7d14a44a5bdd8991074ee868030490ccbf6f5a
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2022-04-20 12:04:11 +0200

    updated templates

---
+++ gbftm_GW_NM150.bat
@@ -0,0 +1,1 @@
+python gbftm.pyw -auto -bg NM100.jpg -nm150 -party -position top -ratio 1.2 -offset 50,180 -text -input -position topleft -offset 80,250 -color 255,196,0 -outcolor 0,0,0 -fontsize 80 -bold
---
+++ gbftm_GW_fa.bat
@@ -0,0 +1,1 @@
+python gbftm.pyw -auto -bg -input -fadein -party -position top -ratio 1.2 -offset 50,180 -element -add fa.png -ratio 3 -position topleft -offset 80,250 -text -input -position topleft -offset 80,350 -color 255,196,0 -outcolor 0,0,0 -fontsize 80 -bold
