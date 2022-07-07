f47f5bcd3176af30848dc20db56618b4182266d7
MizaGBF <47531574+MizaGBF@users.noreply.github.com>
2022-07-07 09:55:29 +0200

    pg template

---
+++ gbftm_proving_grounds.bat
@@ -0,0 +1,1 @@
+python gbftm.py -auto -bg proving -fill -text -input -position topleft -offset 20,20 -fontsize 100 -bold -color 255,255,255 -outcolor 0,0,0 -outsize 8 -party_mainsummon -position topleft -offset 38,433 -ratio 1.25 -text -input -position bottomleft -offset 420,-100 -fontsize 100 -bold
